7c950bc246a1e915e5bcb5998138703eb1aa9e53
HumbertoDevelop <humbertodev14@gmail.com>
2022-03-26 20:50:54 -0400

    done2

--- data/info.js
+++ data/info.js
@@ -2,9 +2,9 @@
 
 [
 	{
-		me: "Humberto García",
-		description:
+		"me": "Humberto García",
+		"description":
 			"I'm a web developer who's beginning to built it's own way into the professional world. My passion is to code. it's a pleasure for me to work in front of my computer building professional websites, analyzing responsive web design issues or fixing bugs. I'll help you to build your own web site with my experience and my knowledge. I'm always looking for new goals",
-		img: "../img/pic3.png",
+		"img": "../img/pic3.png",
 	},
 ];
